45b30ce5339f3a0f73a0645f67fabf42ca83ba2a
Huu Duc <huuduc2707@gmail.com>
2023-07-18 01:14:14 +0700

    Completed exercise 5

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import Exercise4 from "./components/Exercise4";
+import Exercise5 from "./components/Exercise5";
 export default function App(): JSX.Element{
-    return <Exercise4 />;
+    return <Exercise5 />;
 }
---
+++ src/components/Exercise5.tsx
@@ -0,0 +1,18 @@
+import '../style/exercise5.css';
+export default function Exercise5(){
+    function displayText(): void{
+        let inputList = document.getElementsByTagName('input');
+        if(!inputList[0].value || !inputList[1].value) document.getElementById('display-result')!.innerHTML = 'Missing information!';
+        else document.getElementById('display-result')!.innerHTML = `Hello, ${inputList[0].value} ${inputList[1].value}`;
+    }
+    return(
+        <div>
+            <form onSubmit={(e: React.FormEvent):void => e.preventDefault()}>
+                <input type="text" placeholder="First name"/>
+                <input type="text" placeholder="Last name" />
+                <input type="submit" value="GREET ME" onClick={displayText}/>
+            </form>
+            <p id="display-result"></p>
+        </div>
+    );
+}
