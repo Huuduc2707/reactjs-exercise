10335a45596d8a886dff2a2dddac41f1d91cf780
Huu Duc <huuduc2707@gmail.com>
2023-07-17 16:03:00 +0700

    Update for exercise 1

---
+++ src/App.tsx
@@ -0,0 +1,4 @@
+import Exercise1 from "./components/exercise_1";
+export default function App(){
+    return <Exercise1 />;
+}
---
+++ src/components/exercise_1.tsx
@@ -0,0 +1,4 @@
+import '../style/exercise1.css'
+export default function Exercise1(){
+    return <p>Hello, World!</p>;
+}
