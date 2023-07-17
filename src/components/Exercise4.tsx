36e96951ec3d9554422774b877977dbb0ca6ee31
Huu Duc <huuduc2707@gmail.com>
2023-07-18 01:15:36 +0700

    Completed exercise 4

--- src/components/Exercise4.tsx
+++ src/components/Exercise4.tsx
@@ -1,11 +1,11 @@
 import {useState} from 'react';
 import '../style/exercise4.css';
-export default function Exercise4(){
+export default function Exercise4(): JSX.Element{
     const [count, setCount] = useState(0);
     return(
         <div>
             <p>Button has been clicked: {count} times</p>
-            <button onClick={()=>setCount(count+1)}>Click Me</button>
+            <button onClick={(): void => setCount(count+1)}>Click Me</button>
         </div>
     );
 }
