eca77ee28fdeaf6ae1d005cf5c5eef3185c48d57
Huu Duc <huuduc2707@gmail.com>
2023-07-18 09:45:39 +0700

    Completed exercise 5

--- src/components/Exercise5.tsx
+++ src/components/Exercise5.tsx
@@ -1,7 +1,7 @@
 import '../style/exercise5.css';
 export default function Exercise5(): JSX.Element{
     function displayText(): void{
-        let inputList = document.getElementsByTagName('input');
+        let inputList: HTMLCollectionOf<HTMLInputElement> = document.getElementsByTagName('input');
         if(!inputList[0].value || !inputList[1].value) document.getElementById('display-result')!.innerHTML = 'Missing information!';
         else document.getElementById('display-result')!.innerHTML = `Hello, ${inputList[0].value} ${inputList[1].value}`;
     }
