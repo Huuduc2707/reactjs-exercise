122fe4a802d46a83fc54628b3dbf47698dda6f7d
Huu Duc <huuduc2707@gmail.com>
2023-07-19 16:36:09 +0700

    Completed exercise 2

--- src/components/Exercise2.tsx
+++ src/components/Exercise2.tsx
@@ -1,9 +1,11 @@
-import '../style/exercise2.css';
+import styles from '../style/exercise2.module.css';
 let animals: Array<string> = ['🐶 dog', '🐱 cat', '🐔 chicken', '🐮 cow', '🐑 sheep', '🐴 horse'];
 export default function Exercise2(): JSX.Element{
     return (
-        <ul>
-            {animals.map((animal) => <li>{animal}</li>)}
-        </ul>
+        <div className={styles['exercise2-container']}>
+            <ul className={styles['exercise2-ul']}>
+                {animals.map((animal) => <li>{animal}</li>)}
+            </ul>
+        </div>
     );
 }
