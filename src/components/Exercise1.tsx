493b6136ff774777ff497d12f3fa2b8a5abab550
Huu Duc <huuduc2707@gmail.com>
2023-07-19 16:34:04 +0700

    Completed exercise 1

--- src/components/Exercise1.tsx
+++ src/components/Exercise1.tsx
@@ -1,4 +1,8 @@
-import '../style/exercise1.css'
+import styles from '../style/exercise1.module.css'
 export default function Exercise1(): JSX.Element{
-    return <p>Hello, World!</p>;
+    return(
+        <div className={styles['exercise1-container']}>
+            <p className={styles['exercise1-p']}>Hello, World!</p>
+        </div>
+    );
 }
