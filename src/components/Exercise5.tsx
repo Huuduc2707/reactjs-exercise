5f468986421d62e749909b04547b8b0e902e4131
Huu Duc <huuduc2707@gmail.com>
2023-07-19 16:41:55 +0700

    Completed exercise 5

--- src/components/Exercise5.tsx
+++ src/components/Exercise5.tsx
@@ -1,18 +1,28 @@
-import '../style/exercise5.css';
+import React, {useState} from 'react';
+import styles from '../style/exercise5.module.css';
 export default function Exercise5(): JSX.Element{
-    function displayText(): void{
-        let inputList: HTMLCollectionOf<HTMLInputElement> = document.getElementsByTagName('input');
-        if(!inputList[0].value || !inputList[1].value) document.getElementById('display-result')!.innerHTML = 'Missing information!';
-        else document.getElementById('display-result')!.innerHTML = `Hello, ${inputList[0].value} ${inputList[1].value}`;
+    const [result, setResult] = useState('');
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    function updateFirstName(e: React.ChangeEvent<HTMLInputElement>): void{
+        setFirstName(e.target.value);
+    }
+    function updateLastName(e: React.ChangeEvent<HTMLInputElement>): void{
+        setLastName(e.target.value);
+    }
+    function submitHandler(e: React.FormEvent): void{
+        e.preventDefault();
+        if(!firstName || !lastName) setResult('Missing information!');
+        else setResult(`Hello, ${firstName} ${lastName}!`);
     }
     return(
-        <div>
-            <form onSubmit={(e: React.FormEvent):void => e.preventDefault()}>
-                <input type="text" placeholder="First name"/>
-                <input type="text" placeholder="Last name" />
-                <input type="submit" value="GREET ME" onClick={displayText}/>
+        <div className={styles['exercise5-container']}>
+            <form onSubmit={(e)=>submitHandler(e)} className={styles['exercise5-form']}>
+                <input type="text" placeholder='First name' onChange={(e)=>updateFirstName(e)}/>
+                <input type="text" placeholder='Last name' onChange={(e)=>updateLastName(e)}/>
+                <input type="submit" value="GREET ME"/>
             </form>
-            <p id="display-result"></p>
+            <p id="display-result" className={styles['exercise5-p']}>{result}</p>
         </div>
     );
 }
