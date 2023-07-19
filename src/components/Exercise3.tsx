6e7e8144ef8065986ad7a3153acb4a494118d00c
Huu Duc <huuduc2707@gmail.com>
2023-07-19 16:38:41 +0700

    Completed exercise 3

--- src/components/Exercise3.tsx
+++ src/components/Exercise3.tsx
@@ -1,41 +1,42 @@
 import {useState} from 'react';
-import '../style/exercise3.css';
+import styles from '../style/exercise3.module.css';
 let list: Array<string> = ['Apple', 'Banana', 'Tea', 'Coofee'];
-interface CheckboxProp{
+interface CheckboxProps{
     itemName: string;
+    text: string;
+    setText: (text: string)=>void;
 }
-function Checkbox({itemName}: CheckboxProp){
-    const[check, setCheck] = useState(false);
+function Checkbox({itemName, text, setText}: CheckboxProps){
+    const [check, setCheck] = useState(false);
     function checkingHandler(): void{
-        let checkedList: HTMLElement|null = document.getElementById('display-list');
         if(!check){
             setCheck(true);
-            if(checkedList!.textContent === 'Your choice: ') checkedList!.innerHTML += `${itemName}`;
-            else checkedList!.innerHTML += `, ${itemName}`;
+            if(text === 'Your choice: ') setText(`${text}${itemName}`);
+            else setText(`${text}, ${itemName}`);
         }
         else{
             setCheck(false);
-            let str: string|null = checkedList!.textContent;
-            let startIndex: number = str!.indexOf(`${itemName}`);
-            if(startIndex !== 13) checkedList!.innerHTML = str!.slice(0, startIndex - 2) + str!.slice(startIndex + itemName.length);
-            else checkedList!.innerHTML = str!.slice(0, 13) + str!.slice(startIndex + itemName.length + 2);
+            let startIndex: number = text.indexOf(`${itemName}`);
+            if(startIndex !== 13) setText(text.slice(0, startIndex - 2) + text.slice(startIndex + itemName.length));
+            else setText(text.slice(0, 13) + text.slice(startIndex + itemName.length + 2));
         }
     }
-    return <input type="checkbox" checked={check} onChange={checkingHandler}/>;
+    return <input type="checkbox" checked={check} onChange={checkingHandler} className={styles['exercise3-checkbox']}/>;
 }
 export default function Exercise3(): JSX.Element{
+    const [text, setText] = useState('Your choice: ');
     return (
-        <div>
-            <p>Your Checklist:</p>
-            <ul>
+        <div className={styles['exercise3-container']}>
+            <p className={styles['exercise3-p']}>Your Checklist:</p>
+            <ul className={styles['exercise3-ul']}>
                 {list.map((item) =>
                     <li>
-                        <Checkbox itemName = {item}/>
+                        <Checkbox itemName = {item} text = {text} setText = {(text:string)=>setText(text)}/>
                         {item}
                     </li>
                 )}
             </ul>
-            <p id='display-list'>Your choice: </p>
+            <p id={styles['display-list']} className={styles['exercise3-p']}>{text}</p>
         </div>
     );
 }
